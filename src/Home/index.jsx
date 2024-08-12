8aafb17910f61cbbcdbfab2d563150d7b80d8d62
shimuzinho <144175588+shimuzinho@users.noreply.github.com>
2024-08-12 17:34:34 -0400

    Carrosel

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,11 +1,32 @@
 import Header from "../components/Header"
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
 
 export default function Home () {
     
     return (
-        <div>
+        <>
             <Header/>
+            <Carousel
+            infiniteLoop
+            useKeyboardArrows
+            autoPlay
+            showArrows={true}
+            showStatus={false}
+            showThumbs={false}
+            dynamicHeight
+            >
+                <div>
+                    <img src="https://static.netshoes.com.br/bnn/l_netshoes/2024-08-05/1077_maratona_full_exclusivos.png"></img>
+                </div>
+                <div>
+                    <img src="https://static.netshoes.com.br/bnn/l_netshoes/2024-08-05/1077_maratona_full_exclusivos.png"></img>
+                </div>
+                <div>
+                    <img src="https://static.netshoes.com.br/bnn/l_netshoes/2024-08-05/1077_maratona_full_exclusivos.png"></img>
+                </div>
+            </Carousel>
             <h1>Minha Casinha</h1>
-        </div>
+        </>
     ) 
 }
